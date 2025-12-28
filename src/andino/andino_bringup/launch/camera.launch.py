8db0ce46d5a67e82ace377216dad51ffbc532bfe
Mohamed Nasser <mohamed.bn.nasser2001@gmail.com>
2025-12-28 09:30:31 +0200

    Fix URDF dynamic paths and resolve issues in andino_gz

--- src/andino/andino_bringup/launch/camera.launch.py
+++ src/andino/andino_bringup/launch/camera.launch.py
@@ -1,6 +1,7 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.conditions import UnlessCondition
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from os.path import join
@@ -8,6 +9,11 @@ from os.path import join
 pkg = get_package_share_directory('andino_bringup')
 
 def generate_launch_description():
+    use_sim_time = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+        description='Use simulation (Gazebo) clock if true')
+
     intrinsic_params_file = DeclareLaunchArgument(
         'intrinsic_params_file',
         default_value='file://' + join(pkg, 'config', 'raspicam.yaml')
@@ -31,7 +37,7 @@ def generate_launch_description():
     ])
 
     return LaunchDescription([
-        intrinsic_params_file, image_size, fps, fmt,
+        use_sim_time, intrinsic_params_file, image_size, fps, fmt,
         Node(
             package='camera_ros',
             executable='camera_node',
@@ -43,9 +49,10 @@ def generate_launch_description():
                 'width': width,
                 'height': height,
                 'FrameDurationLimits': frame_duration_limits,
-                'frame_id': 'camera_link',
+                'frame_id': 'camera_Link',
                 'camera_info_url': LaunchConfiguration('intrinsic_params_file'),
             }],
+            condition=UnlessCondition(LaunchConfiguration('use_sim_time')),
             remappings=[
                 ('/camera/image_raw', '/image_raw'),
                 ('/camera/camera_info', '/camera_info'),
